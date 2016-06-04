22c0df0613588caf03373334f1501bba55f52cee
thedailyephemera <thedailyephemera@gmail.com>
2016-06-04 15:20:09 -0400

    Added section on classes

---
+++ class/notes.rb
@@ -0,0 +1,10 @@
+#// # Class
+#//
+#// Classes are first-class objects, meaning that they are all instances
+#// of the `Class` class
+#//
+#// Typical instantiation
+class Name
+  # code describing class behavior.
+end
+
