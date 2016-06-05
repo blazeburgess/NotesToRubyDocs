141bfac0222295507b02861af2c48d4ed3c92139
thedailyephemera <thedailyephemera@gmail.com>
2016-06-05 11:26:28 -0400

    Extended section to count method

--- array/notes.rb
+++ array/notes.rb
@@ -243,4 +243,240 @@ elsif tmp = String.try_convert(arg)
 end
 
 #// ## Public Instance Methods
+#//
+#// `&` method
+#//     arr & other_ary --> new_array
+#// Compares elements (using `hash` and `eql?` methods), then returns 
+#// those elements they have in common 
+[ 1, 1, 3, 5 ] & [ 1, 2, 3 ]    #=> [ 1, 3 ]
+[ 'a', 'b', 'b', 'z' ] & [ 'a', 'b', 'c' ] #=> [ 'a', 'b' ]
 
+#// `*`
+#//     array * int  --> new_array
+#//     array * str  --> new_string
+#// The `*int` creates a new array that is the old array repeated `int` 
+#// times. `*str` is equivalent to `array.join(str)`
+[ 1, 2, 3 ] * 3     #=> [ 1, 2, 3, 1, 2, 3, 1, 2, 3 ]
+[ 1, 2 ,3 ] * ","   #=> "1,2,3"
+
+#// `+` method
+#//     array + other_array    --> new_array
+#// Concatenates array and other_array
+[ 1, 2, 3 ] + [ 4, 5 ]    #=> [ 1, 2, 3, 4, 5 ]
+a = [ "a", "b", "c" ]
+c = a + [ "d", "e", "f" ]
+c                         #=> [ "a", "b", "c", "d", "e", "f" ]
+a                         #=> [ "a", "b", "c" ]
+a += [ "d", "e", "f" ]
+a                         #=> [ "a", "b", "c", "d", "e", "f" ]
+
+#// `-` method
+#//     array - other_array  --> new_array
+#// Compares and filters out elements from `array` that are also in 
+#// `other_array`. Creates a new array made of elements that remain.
+#// Comparisons are made with the `hash` and `eql?` methods
+[ 1, 1, 2, 2, 3, 3, 4, 5 ] - [ 1, 2, 4 ] #=> [ 3, 3, 5 ]
+
+#// `<<` method
+#//     array << obj  --> array_modified
+#// Appends value or object to original array. Returns the original array
+#// with updated elements
+[ 1, 2 ] << "c" << "d" << [ 3, 4 ] #=> [ 1, 2, "c", "d", [ 3, 4 ] ]
+
+#// `<=>` method
+#//     array <=> other_array  --> -1 || 0 || +1 || nill
+#// Comprison that returns a numeric value for `array` being less than 
+#// `other_array` (-1), equal to (0), or greater than (1). Returns nil if
+#// not being compared with an array or if comparison returns nil
+[ "a", "a", "c" ] <=> ["a", "b", "c" ]  #=> -1
+[ 1, 2, 3, 4, 5, 6 ] <=> [ 1, 2 ]       #=> 1
+[ 1, 2 ] <=> [ 1, :two ]                #=> nil
+
+#// `==` method
+#//     array == other_array  --> bool
+#// Compares equality, meaning to contain the same number elements and have
+#// each element equal the corresponding element in `other_array`
+[ "a", "c" ] == [ "a", "c", 7 ]      #=> false
+[ "a" "c", 7 ] == [ "a", "c", 7 ]    #=> true
+[ "a" "c", 7 ] == [ "a", "d", "f" ]  #=> false
+[ "a" "c", 7 ] == [ "a", 7, "c" ]    #=> false
+
+#// `[]` method
+#//     array[index]  --> obj || nil
+#//     array[start, length]  --> new_array || nil
+#//     array[range]  --> new_array || nil
+#//     slice(index)  --> new_array || nil
+#//     slice(start, length)  --> new_array || nil
+#//     slice(range)  --> new_array || nil
+#// Element reference at given index or range. Returns elements that meet
+#// the given index(es).
+a = [ "a", "b", "c", "d", "e" ]
+a[2] + a[0] + a[1]      #=> "cab"
+a[6]                    #=> nil
+a[1, 2]                 #=> ["b", "c"]
+a[1..3]                 #=> ["b", "c", "d"]
+a[4..7]                 #=> ["e"]
+a[6..10]                #=> nil
+a[-3, 3]                #=> ["c", "d", "e"]
+
+# special cases
+a[5]                    #=> nil
+a[6, 1]                 #=> nil
+a[5, 1]                 #=> []
+a[5..10]                #=> []
+
+#// `[] =` method
+#//     array[index] = obj  --> obj
+#//     array[start, length] = obj or o_array or nil  --> obj || o_ary || nil
+#//     array[range] = obj or o_array or nil  --> obj || o_array || nil
+#// Various methods of variable assignment. Negative indicies count 
+#// backwards. Non-existent indicies extend the array
+a = Array.new             #=> []
+a[4] = "4"                #=> [nil, nil, nil, nil, "4"]
+a[0,3] = ['a', 'b', 'c']  #=> ["a", "b", "c", nil, "4"]
+a[1..2] = [ 1, 2 ]        #=> ["a", 1, 2, "c", nil, "4"]
+a[0, 2] = "?"             #=> ["?", 2, nil, "4"]
+a[0..2] = "A"             #=> ["A", "4"]
+a[-1] = "Z"               #=> ["A", "Z"]
+a[1..-1] = nil            #=> ["A", nil]
+a[1..-1] = []             #=> ["A"]
+a[0, 0] = [1, 2]          #=> [1, 2, "A"]
+a[3, 0] = "B"             #=> [1, 2, "A", "B"]
+
+#// `any?` method
+#//     array.any? {|obj| block}  --> true || false
+#// Boolean for whether an array has any elements
+a = [1]
+a.any?                    #=> true
+
+b = []
+b.any?                    #=> false
+
+#// `assoc` method
+#//     array.assoc(obj)  --> element_array || nil
+#// Compares passed through `obj` with first element of array (using `==`)
+#// and returns a match (if found) or nil (if not).
+s1 = ["colors", "red", "blue", "green"]
+s2 = [ "letters", "a", "b", "c" ]
+s3 = "foo"
+
+a = [ s1, s2, s3 ]
+a.assoc("letters")    #=> ["letters", "a", "b", "c"]
+a.assoc("foo")        #=> nil
+
+#// `at` method
+#//     array.at(index)  --> obj || nil
+#// Returns the element at `index` or nil if out of range
+a = [ "a", "b", "c", "d", "e" ]
+a.at(0)     #=> "a"
+a.at(-1)    #=> "e"
+
+#// `bsearch` method
+#//     array.bsearch {|x| block}  --> elem || nil
+#// Uses binary search to find a (single) value that meets the block
+#// condition. Since it uses binary search, the array must be sorted
+ary = [0, 4, 7, 10, 12]
+
+array.bsearch {|x| x >= 4}    #=> 4
+array.bsearch {|x| x >= 6}    #=> 7
+array.bsearch {|x| x >= -1}   #=> 0
+array.bsearch {|x| x >= 100}  #=> nil
+
+array.bsearch {|x| 1 - x / 4} #=> 4 || 7
+array.bsearch {|x| 4 - x / 2} #=> nil
+
+#// `bsearch_index` method
+#//     array.bsearch_index {|x| block} --> int || nil
+#// The same as above except it returns the index rather than element.
+ary = [0, 4, 7, 10, 12]
+
+array.bsearch {|x| x >= 4}    #=> 1
+array.bsearch {|x| x >= 6}    #=> 2
+array.bsearch {|x| x >= -1}   #=> 0
+
+#// `clear` method
+#//     array.clear = []
+#// Removes all elements from `array`
+a = [ "a", "b", "c", "d", "e" ]
+a.clear               #=> []
+a                     #=> []
+
+#// `collect` method
+#//     array.collect {|item| block}  --> new_array
+#//     array.collect  --> an_enumerator
+#// Calls the given block on each element of `array`, returns an enumerator
+#// if no block given
+a = [ "a", "b", "c", "d" ]
+
+a.collect { |x| x + "!" }         #=> ["a!", "b!", "c!", "d!"]
+a.map.with_index { |x, i| x * i } #=> ["", "b", "cc", "ddd"]
+a                                 #=> ["a", "b", "c", "d"]
+
+#// `collect!` method
+#//     array.collect! {|item| block}  --> array_modified
+#// Same as above, but destructive of original array
+a = [ "a", "b", "c", "d" ]
+
+a.collect! { |x| x + "!" }             #=> ["a!", "b!", "c!", "d!"]
+a                                      #=> ["a!", "b!", "c!", "d!"]
+a.collect!.with_index { |x,i| x[0..i]} #=> ["", "b", "c!", "d!"]
+a                                      #=> ["", "b", "c!", "d!"]
+
+#// `combination` method
+#//     array.combination(n) {|c| block}  --> array
+#//     array.combination(n)  --> Enumerator
+#// Yields all combinations of length `n` and returns the array. Returns an
+#// enumerator if called without block.
+a = [ 1, 2, 3, 4 ]
+
+a.combination(1).to_a  #=>[[1], [2], [3], [4]]
+a.combination(2).to_a  #->[[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+a.combination(3).to_a  #=> [[1,2,3],[1,2,4],[1,3,4],[2,3,4]]
+a.combination(4).to_a  #=> [[1,2,3,4]]
+a.combination(0).to_a  #=> [[]] // one combination of length 0
+a.combination(5).to_a  #=> []  // no combinations of length 5
+
+#// `compact` method
+#//     array.compact  --> new_array
+#// Returns `self` with nil elements removed
+["a", nil, "b", "c", nil, nil].compact   #=> ["a", "b", "c"]
+
+a = [1, 2, nil, nil, 3, 4]
+a.compact                                #=> [1, 2, 3, 4]
+a                                        #=> [1, 2, nil, nil, 3, 4]
+
+#// `compact!` method
+#//     array.compact!  --> array_modified || nil
+#// Same as above, but detructive and returns `nil` if no nils to clean
+a = [1, 2, nil, nil, 3, 4]
+
+a.compact!                               #=> [1, 2, 3, 4]
+a                                        #=> [1, 2, 3, 4]
+
+b = ["a", "b", "c"]
+b.compact!                               #=> nil
+b                                        #=> ["a", "b", "c"]
+
+#// `concat` method
+#//     array.concat(other_array)  --> array_modified
+#// Appends elements of `other_array` to `array`
+["a", "b"].concat(["c", "d"])   #=> ["a", "b", "c", "d"]
+a = [ 1, 2, 3 ]
+a.concat([ 4, 5 ])              #=> [1, 2, 3, 4, 5]
+a                               #=> [1, 2, 3, 4, 5]
+
+#// `count` method
+#//     array.count  --> int
+#//     array.count(obj)  --> int
+#//     array.count {|item| block}  --> int
+#// Counts number of elements in array. If `obj`, then counts elements that
+#// equal obj (using `==` method). If block, counts elements for which the
+#// block returns true
+array = [1, 2, 4, 2]
+
+array.count                     #=> 4
+array.count(2)                  #=> 2
+array.count(4)                  #=> 1
+array.count {|x| x % 2 == 0}    #=> 3
+
+#// `cycle` method
